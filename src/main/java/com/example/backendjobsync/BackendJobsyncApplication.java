5c1b58189f06396559346f33ad83117c8ded7042
yassine walid <yassine1998walid@gmail.com>
2023-11-27 16:38:25 +0100

    First Commit

---
+++ src/main/java/com/example/backendjobsync/BackendJobsyncApplication.java
@@ -0,0 +1,13 @@
+package com.example.backendjobsync;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendJobsyncApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendJobsyncApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/backendjobsync/BackendJobsyncApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.backendjobsync;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendJobsyncApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
